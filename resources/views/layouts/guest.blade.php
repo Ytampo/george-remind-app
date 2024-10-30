c2465c71cce46bc5d1893e6b2c7d8680b0082a1f
Ytampo <angerball8@gmail.com>
2024-10-30 21:31:46 +0900

    fix: guest.bladeのheadタグ修正&レイアウト微修正

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,19 +3,24 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    
+        <link rel="icon" href="{{ asset('img/logo-fire.jpg') }}">
+        <title>単位危機管理</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap');
+        </style>
 
         <!-- Scripts -->
         <link rel="stylesheet" href="{{ asset('css/login.css') }}">
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
     <body class="font-sans text-gray-900 antialiased">
+        <!-- iPhone ノッチ部分色付け -->
+        <div class="notch-bar"></div>
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900" id="login-container">
             <div class="login-logo">
                 <img src="{{ asset('img/logo-fire-rad.png') }}" alt="logo">
